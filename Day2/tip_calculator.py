139a235c3d425cf0f09aec96e56f9798fcad2e63
Fahad Al Maashani <122757915+Fahad-Al-Maashani@users.noreply.github.com>
2025-01-19 14:31:28 +0400

    Update tip_calculator.py

--- Day2/tip_calculator.py
+++ Day2/tip_calculator.py
@@ -1,1 +1,10 @@
+print ("Welocme to the tip calculator! ") 
+bill =float (input ("What was the total bill ? "))
+tip=int(input("How much tip would you like to give ? 10,12, or 15? 12 ")) 
+people = int(input ("How many people to split the bill ")) 
+perc= tip/100 
+total_perc = bill *perc
+total= total_perc+bill 
+per_person= total/people
+print (round (per_person,3))
 
